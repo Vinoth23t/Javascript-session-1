283859a2311a3b1f9f74b3b91a00a106120e960a
Vinoth23t <120643205+Vinoth23t@users.noreply.github.com>
2022-12-25 19:38:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+//Create variables for given data types
+//String
+var t ="My first Js program"
+console.log(t)
+
+//Int
+var a=45
+console.log(a)
+
+//Boolean
+var s=false
+console.log(s)
+
+//float
+var g=56.3
+console.log(g)
+
+
+//Area of a circle
+{
+var Area
+var radius=8
+let pi=3.14
+ Area=pi*radius*radius
+console.log(Area)
+}
+//Circumference of a circle
+{
+    let radius=14
+var pi=3.14
+var Circumference
+Circumference=2*pi*radius
+console.log(Circumference)
+}
+
+
+//Radius of a circle
+{
+
+const pi=3.14
+let c= 40
+var radius
+radius=c/2*pi
+console.log(radius)
+    }
